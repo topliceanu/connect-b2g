89ca874a384d31ba0358c45c57ee06f52a4febfe
alexandru topliceanu <alext@vibetrace.com>
2012-07-02 22:43:00 -0700

    simplify testcase by adding supertest lib

--- test/index.js
+++ test/index.js
@@ -1,45 +1,25 @@
 var fs = require('fs');
 var path = require('path');
 var connect = require('connect');
-var request = require('request');
+var request = require('supertest');
 
 var b2g = require('../');
 
 var manifestPath = 'test/fixtures/manifest.webapp';
-debugger;
-var app
-
+var app = connect();
+app.use( b2g(manifestPath) )
 
 describe( 'connect-b2g()', function () {
-
-	before( function (done) {
-		app = connect().use(b2g(manifestPath)).listen(3000);
-		done();
-	});
-
-	it('should server the correct manifest file', function (done) {
+	it('should server the correct manifest file with correct Content-Type header', function (done) {
 		fs.readFile( manifestPath, 'UTF-8', function (err, fileContents) {
 			if (err) return done(err);
-			request.get('http://127.0.0.1:3000/manifest.webapp', function (err, response) {
-				if (err) return done(err);
-				if (fileContents !== response.body) return done(Error('not the same content'));
-				return done();
-			});
+			request(app)
+				.get('/manifest.webapp')
+				.set('Accept', 'application/x-web-app-manifest+json')
+				.expect('Content-Type', 'application/x-web-app-manifest+json')
+				.expect(200)
+				.expect(fileContents)
+				.end(done);
 		});
 	});
-
-	it('should serve the manifest using the correct Content-Type header', function (done) {
-		request.get('http://127.0.0.1:3000/manifest.webapp', function (err, response) {
-			if (err) return done(err);
-			if (response.headers['content-type'] !== 'application/x-web-app-manifest+json') 
-				return done(Error('unexpected header'));
-			return done();
-		});
-	});
-
-	after( function (done) {
-		app.close();
-		done();
-	});
-
 });
